6894ab0d8ba932110024819cb1b665165bdb1d5a
lbt <908236075@qq.com>
2026-05-04 12:07:42 +0800

    主要文件

---
+++ js/demo/index.js
@@ -0,0 +1,8 @@
+let a;
+// a=9;
+function count(x, y) {
+    return x + y;
+}
+let result = count(3, 8);
+console.log(result);
+export {};
---
+++ js/demo/index.ts
@@ -0,0 +1,9 @@
+let a: string
+// a=9;
+function count(x:number,y:number){
+  return x+y;
+}
+
+let result = count(3, 8);
+console.log(result);
+
